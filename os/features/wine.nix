6729d80caefd6a8a5ab3bb0679cade9b903b5d43
MindednessKind <mindednesskind@gmail.com>
2026-06-30 22:59:36 +0800

    在尝试修复wine任务栏显示问题

--- os/features/wine.nix
+++ os/features/wine.nix
@@ -2,6 +2,7 @@
 {
   flake.nixosModules.wine = { pkgs, ... }: {
     programs.nix-ld.enable = true;
+    services.xembed-sni-proxy.enable = true;
 
     environment.systemPackages = with pkgs; [
       wineWow64Packages.stableFull

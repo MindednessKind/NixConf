a0760f2d611c204f723669a9659478b6b25f3182
MindednessKind <mindednesskind@gmail.com>
2026-06-30 23:16:54 +0800

    忽略了部分错误设置值

--- os/features/wine.nix
+++ os/features/wine.nix
@@ -2,7 +2,7 @@
 {
   flake.nixosModules.wine = { pkgs, ... }: {
     programs.nix-ld.enable = true;
-    services.xembed-sni-proxy.enable = true;
+    # services.xembed-sni-proxy.enable = true;
 
     environment.systemPackages = with pkgs; [
       wineWow64Packages.stableFull
